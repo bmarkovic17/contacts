9d8169a00d0bb5b5542de404447ca56ae2ca276a
Boris Marković <bmarkovic17@outlook.com>
2020-12-21 00:19:01 +0100

    Add integration test for create contact endpoint

--- ContactsApiTests/ContactsControllerIntegrationTest.cs
+++ ContactsApiTests/ContactsControllerIntegrationTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using ContactsApi.Dtos;
 using Xunit;
 
 namespace ContactsApiTests
@@ -39,6 +42,46 @@ namespace ContactsApiTests
             // Assert
             Assert.True(response.StatusCode == HttpStatusCode.NotFound);
             Assert.False(string.IsNullOrEmpty(responseValue));
+        }
+
+        [Fact]
+        public async Task PostNonExistingContactAsync()
+        {
+            // Arrange
+            var endpoint = "contacts";
+            // Arrange
+            var postContact = new PostContactDto
+            {
+                FirstName = "Bill",
+                Surname = "Gates",
+                DateOfBirth = new DateTime(1955, 10, 28),
+                Street = "Pointe Lane",
+                AddressNumber = "4597",
+                Postcode = "33308",
+                City = "Fort Lauderdale",
+                Country = "Florida, US",
+                ContactData = new List<PostContactDataDto>
+                {
+                    new PostContactDataDto
+                    {
+                        ContactDataType = "PHONE",
+                        ContactDataValue = "0901000100"
+                    },
+                    new PostContactDataDto
+                    {
+                        ContactDataType = "MAIL",
+                        ContactDataValue = "bill.gates@mail.com"
+                    }
+                }
+            };
+
+            // Act
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync(endpoint, postContact);
+            ContactDto contact = await response.Content.ReadFromJsonAsync<ContactDto>();
+
+            // Assert
+            Assert.True(response.StatusCode == HttpStatusCode.Created);
+            Assert.True(contact.Id > 0);
         }
     }
 }
